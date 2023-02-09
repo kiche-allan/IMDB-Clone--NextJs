4cc63c141743f102c6bfc26430d17b905a1cee01
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-09 19:11:04 +0300

    icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import MenuItem from './MenuItem'
 import {AiFillHome} from "react-icons/ai"
+import {BsInfoCircleFill} from "react-icons/bs"
+
 
 export default function Header() {
   return (
     <div> 
         <div className="flex justify-between items-center py-4 px-10 bg-black text-white">
              <MenuItem  title= "HOME" address = "/" Icon ={AiFillHome} />
+             <MenuItem  title= "HOME" address = "/about" Icon ={BsInfoCircleFill} />
         </div>
 
         <div className="flex justify-between items-center py-2 px-10 bg-black text-white">
