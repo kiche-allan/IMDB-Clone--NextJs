9a4201dfb12060a9e88f790df249dd4a3fa174d4
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-09 18:46:01 +0300

    header3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import MenuItem from './MenuItem'
+import {AiFillHome} from "react-icons/ai"
 
 export default function Header() {
   return (
     <div> 
         <div className="flex justify-between items-center py-4 px-10 bg-black text-white">
-
+             <MenuItem  title= "HOME" address = "/" Icon ={AiFillHome} />
         </div>
 
         <div className="flex justify-between items-center py-2 px-10 bg-black text-white">
 
-            
+
         </div>
     </div>
   )
