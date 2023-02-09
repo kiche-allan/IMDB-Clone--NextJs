de77bfd3a36516cf189c0a0d0aa9929305bf16c5
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-09 18:29:31 +0300

    header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,22 @@
+import Header from '@/components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body>{children}</body>
+      <body>
+        
+        {/* <Head /> */}
+        <Header />
+
+        {/*Navbar */}
+
+
+        {/*SearchBox */}
+
+
+        {children}</body>
     </html>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div> 
+        <div className="flex justify-between items-center py-4 px-10 bg-black text-white">
+
+        </div>
+
+        <div className="flex justify-between items-center py-2 px-10 bg-black text-white">
+
+            
+        </div>
+    </div>
+  )
+}
