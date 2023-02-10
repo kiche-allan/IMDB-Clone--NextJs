ed535ef3332dad821a5c8da8aa0726c19e1f3b1a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-10 15:37:50 +0300

    page4

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+ import React from 'react'
+ 
+ export default function about() {
+   return (
+     <div className='max-w-6xl mx-auto space-x-4'>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, laboriosam placeat! Eveniet assumenda sapiente beatae quod earum rem. Repudiandae aspernatur iusto alias dicta veritatis a doloremque corporis suscipit voluptatibus nam? Explicabo ex tempora, voluptatibus iste hic nobis id, ea ratione, enim voluptas est. Dignissimos dolorum porro, at recusandae consectetur nisi vel, natus, non commodi ne </p>
+        <p className=''>mo aperiam. Voluptatum repudiandae quisquam autem eligendi! Eius culpa adipisci ex, molestiae odio hic. Non pariatur tempore enim beatae perspiciatis cumque iusto porro magni dolor repellat mollitia a error dignissimos fugiat accusamus quo labore, officia sapiente culpa odio doloremque perferendis, nobis magnam! Quaerat at expedita dignissimos quasi pariatur deserunt! Porro laudantium assumenda ratione explicabo doloremque temporibus eaque at excepturi, molestias in modi molestiae vitae quasi ducimus quia necessitatibus fugiat veniam itaque dolore ipsa asperiores, minima est? Accusamus molestias, enim corporis repellat </p>
+
+        <p>laudantium deserunt qui in ullam asperiores debitis. Earum veritatis praesentium accusamus eius delectus eos eum laborum et maiores aperiam ut quasi quidem fugit nam sequi a quis tenetur porro expedita, ad quia quod reprehenderit! Neque veniam, deserunt voluptatum eveniet ipsa quod fugiat repellat molestiae dolorem earum nulla consequatur ducimus aperiam. Officiis possimus harum molestiae, nemo modi nihil voluptas sapiente enim eius, ex repudiandae animi, voluptate laborum tempora odio iusto sed ullam atque! Totam iusto exercitationem cumque. Facilis, doloribus? Officia cum maiores doloremque eveniet labore excepturi mollitia saepe ab assumenda, corrupti ut consectetur consequatur, totam ducimus autem cupiditate blanditiis, unde rem neque</p>
+
+        <p> tempora quasi omnis. Quae fugit unde fuga ut quidem. Minima ipsum commodi maxime provident ratione deserunt vitae, fugiat id. Exercitationem ullam impedit, praesentium perspiciatis aspernatur dolorum, ratione sit alias totam blanditiis at culpa beatae dolor corporis atque error dolores repellat consequuntur reiciendis! Quia obcaecati eius error tempora eos assumenda dolores ipsum, aspernatur earum ea accusamus, facilis fuga itaque! Porro similique sunt explicabo voluptatem ipsum.</p>
+     </div>
+   )
+ }
+ 
