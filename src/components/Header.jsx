792beac0dd613cf7c8212a1fcbcea00c9550f523
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-10 09:04:58 +0300

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ import {BsInfoCircleFill} from "react-icons/bs"
 export default function Header() {
   return (
     <div> 
-        <div className="flex justify-between items-center py-4 px-10 bg-black text-white">
+        <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center">
              <MenuItem  title= "HOME" address = "/" Icon ={AiFillHome} />
-             <MenuItem  title= "HOME" address = "/about" Icon ={BsInfoCircleFill} />
+             <MenuItem  title= "ABOUT" address = "/about" Icon ={BsInfoCircleFill} />
         </div>
 
         <div className="flex justify-between items-center py-2 px-10 bg-black text-white">
