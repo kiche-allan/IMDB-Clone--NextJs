db8aee803c376aac2f50ebc6cf8fec791414bb19
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-14 01:51:00 +0300

    navbaritem

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import {useSearchParams} from 'next/navigation'
 
 export default function NavbarItem({title, param}) {
   return (
     <div>
-        <Link href = {` /?genre = ${param}`}>
+        <Link className='m-4 hover:text-amber-600 font-semibold p-2' href = {` /?genre = ${param}`}>
             {title}
         </Link>
     </div>
